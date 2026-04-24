aabbf68eb7db5001b08b70af72845b27b2c04562
sashi <wunnava.sashi@gmail.com>
2026-04-24 14:26:56 -0400

    Practice PR 3

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,6 +7,7 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
   console.log('has title');
+  console.log("Practice PR 1");
 });
 
 test('get started link', async ({ page }) => {
